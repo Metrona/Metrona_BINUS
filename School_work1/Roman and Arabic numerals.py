0b3c5ec57bb7a23dca12938f756248cbdb131d53
Metrona <Metrona@users.noreply.github.com>
2019-09-25 15:41:48 +0700

    Update Roman and Arabic numerals.py

--- School_work1/Roman and Arabic numerals.py
+++ School_work1/Roman and Arabic numerals.py
@@ -7,7 +7,7 @@ def convertnum(romnum):
         elif(num == "V"):
             x += 5        
         elif(num == "I"):
-            if(romnum == "IV" or romnum == "XIV" or romnum == "IX"):
+            if(romnum[-1] == "V" or romnum[-1] == "X"):
                 x -= 1
             else:
                 x += 1
@@ -20,12 +20,12 @@ def convertrom(num):
     y = ""
     if(len(str(num)) == 2):
         y += int(num/10)*"X"
-    if(9 > num >= 5 or 19 > num >= 15):
+    if(9 > num%10 >= 5):
         y += "V"
     if("9" in str(num)):
         y += "IX"
     if("4" in str(num)):
         y += "IV"
-    if(4 > num >= 1 or 9 > num >= 6 or 14 > num >= 11 or 19 > num >= 16):
+    if(num%5 != 0 and num%5 != 4):
         y += num%5*"I" 
     return print(y)
